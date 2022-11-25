49f4f8dc10ddb20ed9dd9eea91fdb0ad0e48f837
Tomitom13 <thomas.buton@isen.yncrea.fr>
2022-11-25 15:04:58 +0100

    beautify resolve.sh and readme. Fix some regex

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# NucleiTemplate-RootMe
-list of template to resolve rootme challenge
+# Nuclei Templates for RootMe
+
+This template is used to resolve challenges on the rootme website.
+
+## How to use
+
+1. Connect to the rootme website and get the challenge number (as in the url)
+2. Run `./resolve.sh` to resolve all the challenges
+3. Or run `./resolve.sh <challenge_number>` to resolve only one challenge
+
+## How to contribute
+
+1. Fork the project
+2. Create a branch
+3. Commit your changes
+4. Push to the branch
+5. Create a Pull Request
+
+## Other
+
+Feel free to contribute to this project by adding new templates or improving existing ones. For any questions or tips, feel free to contact me on Twitter [@TomitoMoinsDeux](https://twitter.com/TomitoMoinsDeux).
--- resolve.sh
+++ resolve.sh
@@ -4,9 +4,48 @@
 
 #!/bin/bash
 
+# if no argument is given, use the default list
 # list of numbers [1, 2, 52, 68]
-nums=(1 2 5 52 68)
+nums=(1 2 5 21 52 68)
+# if one argument is given, verify that it is a number, and use it as the list
+if [ $# -eq 1 ]; then
+    if [[ $1 =~ ^[0-9]+$ ]]; then
+        # if its in the list, use it
+        # else, print an error message and exit
+        if [[ " ${nums[@]} " =~ " $1 " ]]; then
+            nums=($1)
+        else
+            echo "Error: $1 is not implemented yet"
+            exit 1
+        fi
+
+    else
+        echo "Argument must be a number"
+        exit 1
+    fi
+else if [ $# -gt 1 ]; then
+    echo "Too many arguments"
+    exit 1
+fi
+fi
+
+
 
 for nb in ${nums[@]}; do
-    nuclei -u "http://challenge01.root-me.org/web-serveur/ch${nb}/" -t Web-server/ -silent
+    # print the challenge number stylized with colors in boxes 
+    # ================
+    # =  CHALLENGE 1 =
+    # ================
+    #  in orange color 
+    echo -e "\e[1;33m================\e[0m"
+    echo -e "\e[1;33m= CHALLENGE $nb =\e[0m"
+    echo -e "\e[1;33m================\e[0m"
+
+    # execute nuclei command
+
+    if [ $nb -eq 21 ]; then
+        nuclei -u "http://challenge01.root-me.org/web-serveur/ch${nb}/" -t Web-server/ -silent
+    else
+        nuclei -u "http://challenge01.root-me.org/web-serveur/ch${nb}/" -t Web-server/ -eid "File_upload-Type_MIME" -silent
+    fi
 done
